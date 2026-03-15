dfe352f415ec0ef60cfa5944f56727ba5e6bbb9e
soulcode <graigdev@gmail.com>
2026-03-15 14:51:45 +0000

    Part 2 non-coastal city

---
+++ part2/target/debug/deps/part2-82ad500b742fd4f0.d
@@ -0,0 +1,5 @@
+/home/soulcode/Documents/Rust/Exercices/frontendmaster/part2/target/debug/deps/part2-82ad500b742fd4f0.d: src/main.rs
+
+/home/soulcode/Documents/Rust/Exercices/frontendmaster/part2/target/debug/deps/part2-82ad500b742fd4f0: src/main.rs
+
+src/main.rs:
---
+++ part2/target/debug/part2.d
@@ -0,0 +1,1 @@
+/home/soulcode/Documents/Rust/Exercices/frontendmaster/part2/target/debug/part2: /home/soulcode/Documents/Rust/Exercices/frontendmaster/part2/src/main.rs
